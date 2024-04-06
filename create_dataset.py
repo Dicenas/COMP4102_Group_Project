5f43e98c218b71fee02209dddbb0d2a03a1481ce
Dicenas <d_godfrey1@aol.com>
2024-04-06 14:27:35 -0400

    added lighting correction to preprocessing

--- create_dataset.py
+++ create_dataset.py
@@ -4,15 +4,53 @@ import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
 
+def apply_lighting_correction(image_path):
+    """
+    Apply histogram equalization to an image for lighting correction.
+    Args:
+    - image_path (str or Path): The path to the input image.
+    
+    Returns:
+    - corrected_image: The lighting-corrected image.
+    """
+    # Load the image in grayscale mode
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+    # Apply histogram equalization
+    corrected_image = cv2.equalizeHist(image)
+    
+    return corrected_image
+
+def process_dataset(dataset_folder, output_folder):
+    """
+    Apply lighting correction to all images in the dataset folder and save the processed images.
+    Args:
+    - dataset_folder (str or Path): The folder containing the dataset images.
+    - output_folder (str or Path): The folder where corrected images will be saved.
+    """
+
+    for dir_ in os.listdir(dataset_folder):
+        for img in os.listdir(os.path.join(dataset_folder, dir_)):
+            img_path = os.path.join(dataset_folder, dir_, img)
+
+            if not os.path.exists(os.path.join(output_folder, dir_)): # Create directory if it doesn't already exist
+                os.makedirs(os.path.join(output_folder, dir_))
+            
+            corrected_image = apply_lighting_correction(img_path)
+
+            # Save the corrected image
+            output_image_path = os.path.join(output_folder, dir_, img)
+            success = cv2.imwrite(output_image_path, corrected_image)
+            if success:
+                print(f"Processed and saved: {output_image_path}")
+            else:
+                print("Failed to save image.")
+
+
+
 DATA_DIR = './data'
+LIGHTING_CORRECTION = './preprocessing/lighting'
+process_dataset(DATA_DIR, LIGHTING_CORRECTION)
 
-for dir_ in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir))[:1]:
-        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        plt.show()
-        plt.imshow(img_rgb)
-
-plt.show()
 
